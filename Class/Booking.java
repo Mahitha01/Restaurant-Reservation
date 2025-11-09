b2f21760df6d8f3ba672321e887e1960548c6c0d
Tommy Wei <wei487@purdue.edu>
2025-11-09 17:12:31 -0500

    Booking class
    Extends user and allows a user to book a time day and table

---
+++ Class/Booking.java
@@ -0,0 +1,53 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class Booking  extends User implements BookingInterface{
+    private int partySize;
+    private String bookingTime;
+    private ArrayList<String> tables; //in format of #table number, #table Size, #availble or not
+    private boolean isAvailable;
+
+    public Booking(String email, String password, int partySize, String bookingTime) {
+        super(email, password);
+        this.partySize = partySize;
+        this.bookingTime = bookingTime;
+    }
+
+    public int getPartySize() {
+        return partySize;
+    }
+
+    public String getBookingTime() {
+        return bookingTime;
+    }
+    public void setPartySize(int partySize) {
+        this.partySize = partySize;
+    }
+
+    public void setBookingTime(String bookingTime) {
+        this.bookingTime = bookingTime;
+    }
+
+    public void setTables(ArrayList<String> tables) {
+        this.tables = tables;
+    }
+
+    public boolean isAvailable(int target) { // {3,4,true}
+        if (tables == null) {
+            return false;
+        }
+            for (String table : tables) {
+                String[] line = table.split(",");
+                int tableNumber = Integer.parseInt(line[0]);
+                if (tableNumber == target && Boolean.parseBoolean(line[2])) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+
+
+
+}
