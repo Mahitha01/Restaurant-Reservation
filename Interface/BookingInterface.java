7f0700acbc4e6a1cd35d0332f39caa90d2b9cfb4
Tommy Wei <wei487@purdue.edu>
2025-11-09 17:13:23 -0500

    Add methods for booking management in interface

--- Interface/BookingInterface.java
+++ Interface/BookingInterface.java
@@ -1,1 +1,10 @@
+import java.util.ArrayList;
 
+public interface BookingInterface {
+    int getPartySize();
+    String getBookingTime();
+    void setPartySize(int partySize);
+    void setBookingTime(String bookingTime);
+    void setTables(ArrayList<String> tables);
+    boolean isAvailable(int target);
+}
