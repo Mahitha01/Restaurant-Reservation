020babce345ab4fa99001383cc2ee35c11199120
Tommy Wei <wei487@purdue.edu>
2025-11-09 17:11:23 -0500

    Update user.java

--- Class/user.java
+++ Class/user.java
@@ -1,1 +1,25 @@
+/**
+ * User Class
+ * Creates a new User for account creation in Client and Server class
+ * Also can use for deletion as well
+ * A user would have an Email and Password, that can be checked by iterating through a list of
+ * users in a .txt file
+ */
+public class User implements UserInterface{
+    private String email;
+    private String password;
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
