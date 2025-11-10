2d1805bc8c3d1924889383d7eac5275cf3795515
mahitha01 <smahithakodali@gmail.com>
2025-11-09 19:22:37 -0500

    User and Booking tests

---
+++ TestCases/UserUnitTest.java
@@ -0,0 +1,19 @@
+package database.TestCases;
+import database.Class.User;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * This tests the user class
+ *
+ * @author Mahitha Kodali , lab sec 02
+ * @version Nov 7, 2025
+ */
+public class UserUnitTest {
+    @Test
+    public void testConstructor() {
+        User user = new User("test@gmail.com", "test123");
+        assertEquals("test@gmail.com", user.getEmail());
+        assertEquals("test123", user.getPassword());
+    }
+}
+
